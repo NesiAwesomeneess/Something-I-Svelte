e6c51ae9b98a4166c806bd726ea682862044eae7
Nesi <okoyomohnesi@gmail.com>
2023-07-14 12:41:33 +0100

    refactored the code
    The code is fully functional now

--- Todo-app/src/lib/entry.svelte
+++ Todo-app/src/lib/entry.svelte
@@ -1,48 +1,62 @@
 <script>
-    import {createEventDispatcher} from 'svelte'
+    export let completed = false
+    export let id
+    export let task = "new"
+    let newTask = task
 
-    const dispatcher = createEventDispatcher()
-
-    export let entry = {task: "new", id: 0, completed: false}
-    export let task = entry.task
-    let editing = false
-
-    function initialEntryEdit(){
-        editing = true
-    }
-
-    function finishEntryEdit(){
-        editing = false
-        dispatcher("edit", {entry: entry})
+    function finishedEdit(){
+        if (newTask){
+            task = newTask
+            return
+        }
+        newTask = task
+        
+// I just put this here because I was tired of the error
+        id = id
     }
 
 </script>
 
 <!-- this is a list entry essentially -->
 <li>
-    <input type="checkbox" bind:checked={entry.completed}/>
-    {#if editing}
-        <input class="edit-entry"
-        bind:value={task}
-        on:blur={finishEntryEdit}/>
+    <input type="checkbox" bind:checked={completed}/>
+    {#if completed}
+    <span class="completed-task">
+        {task}
+    </span>
     {:else}
-        <button class="entry" 
-        on:click={initialEntryEdit}>
-        {entry.task}
-        </button>
+        <input class="entry"
+        bind:value={newTask}
+        on:focus={(event) => event.target.select()}
+        on:blur={finishedEdit}
+        on:keypress={(event) => {if (event.key === "Enter"){
+            finishedEdit()
+        }}}/>
     {/if}
+
+
 </li>
 
 <style>
-    .edit-entry {
-        position: absolute;
-    }
+
 
     .entry {
         font-size: 1rem;
+        resize: horizontal;
 
+        border: none;
         border-style: none;
         background-color: rgba(240, 248, 255, 0);
         padding: 2px;
     }
+
+    .entry:focus {
+        margin: 2px;
+        resize: horizontal;
+    }
+
+    .completed-task {
+        background-color: gray;
+    }
+
 </style>
--- Todo-app/src/routes/+page.svelte
+++ Todo-app/src/routes/+page.svelte
@@ -4,6 +4,9 @@
     let todos = [{task : "sharpen spear", id : 0, completed: false}]
     let newTask = ''
 
+    let mouseX = 0
+    let mouseY = 0
+
     function addTodo(){
         if (newTask){
             todos = [...todos, {task: newTask, id: crypto.randomUUID()}]
@@ -15,6 +18,12 @@
         todos = todos.filter(({completed}) => {return !(completed)});
     }
 
+    window.onmousemove = e => {
+        mouseX = e.clientX
+        mouseY = e.clientY
+    }
+
+
 </script>
 
 <main>
@@ -22,20 +31,26 @@
 
     <ul class="todo-list">
         {#each todos as entry (entry.id)}
-            <Entry entry={entry} 
+            <Entry id={entry.id} 
             bind:task={entry.task}
-            />
+            bind:completed={entry.completed}/>
         {/each}
-        <input bind:value={newTask} on:blur={addTodo}/>
+
+        <input 
+        bind:value={newTask} 
+        on:blur={addTodo} 
+        on:keypress={(event) => {if (event.key === "Enter"){
+            addTodo()
+        }}}/>
+
         <button on:click={taskCompleted}>clear completed</button>
     </ul>
+
+    <div class="mouse-trail" x="{mouseX}px"></div>
 
 </main>
 
 <style>
-    * {
-        font-family: Arial, Helvetica, sans-serif;
-    }
 
     h1{
         margin: 20px;
@@ -43,6 +58,22 @@
 
     .todo-list{
         list-style: none;
+    }
+
+    .mouse-trail {
+        position: fixed;
+        left: var(x);
+        top: 0px;
+        z-index: 200;
+
+        background-color: rgb(53, 52, 52);
+
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+
+        pointer-events: none;
+
     }
     
 </style>
