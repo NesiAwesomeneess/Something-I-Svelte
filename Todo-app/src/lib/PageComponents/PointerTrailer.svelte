2c488d1a8c9206d011e8a7897603db40603e8139
Nesi <okoyomohnesi@gmail.com>
2023-07-27 18:19:42 +0100

    Finally animated the trailer

--- Todo-app/src/lib/PageComponents/PointerTrailer.svelte
+++ Todo-app/src/lib/PageComponents/PointerTrailer.svelte
@@ -1,34 +1,94 @@
 <script>
     import { spring } from 'svelte/motion';
+    import RiveComponent from '../RiveComponent.svelte';
     
     let coords = spring({x: 0, y: 0}, {
-		stiffness: 0.02,
-		damping: 0.2
-    })
-
+        stiffness: 0.02,
+        damping: 0.2
+    });
     export let mousePosition = {x: 0, y: 0}
-    $: coords.set({x: mousePosition.x, y: mousePosition.y})
+    $: {
+        coords.set({x: mousePosition.x, y: mousePosition.y});
+        checkContext();
+    }
+    
+    let size = spring(24, {
+        stiffness: 0.02,
+        damping: 0.2
+    });
 
-    let size = 30
+
+    export let currentContext = "null";
+    let animationInputs;
+
+    function checkContext(){
+        if (animationInputs){
+            if (currentContext === "null"){
+                animationInputs[0].fire()
+                size.set(24);
+            }
+            else{
+                size.set(42);
+                
+                switch (currentContext){
+                    case "entry-input":
+                        animationInputs[1].fire()
+                        break;
+                    case "checkbox":
+                        animationInputs[2].fire()
+                        break;
+                    case "entry-edit":
+                        animationInputs[3].fire()
+                        break;
+                    case "bookmark":
+                        animationInputs[4].fire()
+                        break;
+                }
+            }
+        }
+    }
+        
+
 
 </script>
 
-<div id="mouse-trail" 
-    style="width: {size + 8}px; height : {size + 8}px;
-    left: {$coords.x + (size / 2)}px; 
-    top: {$coords.y - (size)}px;">
+<div id="trail-outline" 
+    style="width: {$size + 8}px; height : {$size + 8}px;
+    left: {$coords.x + ($size / 2)}px; 
+    top: {$coords.y - ($size) - 10}px;">
     
-    <div id="trail-outline" 
+    <div id="mouse-trail" 
         style="
-        width : {size}px; 
-        height : {size}px;
+        position: relative;
+        width : {$size}px; 
+        height : {$size}px;
         left: {4}px;
         top: {4}px;">
+
+        <div style="
+        position: absolute;
+        padding: 0;
+        top: 50%;
+        left: calc(50% - 1px);
+        transform: translateX(-50%) translateY(-50%);
+        width: 30px; 
+        height: 30px;">
+
+            <RiveComponent
+                src='/icons.riv' 
+                autoplay={true}
+                artboard='Icons'
+                stateMachines={'State Machine 1'}
+                bind:riveInputs={animationInputs}
+                canvasNumber=1
+                />
+        </div>
     </div>
+
 </div>
 
 <style>
-    #mouse-trail {
+    #trail-outline {
         position: fixed;
         z-index: 200;
 
@@ -39,9 +99,9 @@
         box-shadow: 5px 5px 5px -2px rgba(0, 0, 0, 0.094);
     }
     
-    #trail-outline {
+    #mouse-trail {
         position: absolute;
-        z-index: -2;
+        z-index: 0;
         
         border-radius: 50%;
         background-color: #FFAF85;
